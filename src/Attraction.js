5f985f233272bc4f724a628de53fe14a6f2a3be5
Phillip Duarte <phillipmd1707@gmail.com>
2021-12-05 20:39:52 -0500

    Added 3 Attractions

--- src/Attraction.js
+++
@@ -1,5 +0,0 @@
-class Attraction extends HTMLElement {
-    constructor() {
-
-    }
-}
---
+++ src/script.js
@@ -0,0 +1,53 @@
+class TouristAttraction extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const projectInfoContainer = document.createElement('p');
+        projectInfoContainer.innerHTML = `${this.getAttribute('name')}`;
+        const icon = document.createElement('img');
+        icon.src = `${this.getAttribute('src')}`;
+        this.appendChild(projectInfoContainer);
+        this.appendChild(icon);
+    }
+}
+
+let attractionArray = [
+    {
+        "hasVisited" : false,//Boolean that represents if user has visited before
+        "name" : "Philadelphia Museum of Art",
+        "food" : true,
+        "free" : false,//If it's free it's true, if it costs anything it's false
+        "setting" : "Indoors",
+        "address" : "2600 Benjamin Franklin Pkwy, Philadelphia, PA 19130",
+        "type" : "Museum",
+        "area" : "Center City",
+        "daysOpen" : ["Sunday", "Monday", "Thursday", "Friday", "Saturday"]
+    },
+    {
+        "hasVisited" : false,
+        "name" : "Franklin Institute",
+        "food" : true,
+        "free" : false,
+        "setting" : "Indoors",
+        "address" : "222 N 20th St, Philadelphia, PA 19103",
+        "type" : "Museum",
+        "area" : "Center City",
+        "daysOpen" : ["Sunday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    },
+    {
+        "hasVisited" : false,
+        "name" : "Eastern State Penitentiary",
+        "food" : false,
+        "free" : false,
+        "setting" : "Mostly Indoors",
+        "address" : "2027 Fairmount Ave, Philadelphia, PA 19130",
+        "type" : "Historical",
+        "area" : "North Philadelphia",
+        "daysOpen" : ["Sunday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    }
+]
+
+customElements.define('tourist-attraction', TouristAttraction)
+
